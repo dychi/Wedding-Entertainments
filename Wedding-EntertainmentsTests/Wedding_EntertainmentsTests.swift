d578dab89cc441d80cbc787cad973676b09b6350
Daichi Yamaoka <tnychor06@gmail.com>
2019-03-08 02:50:27 +0900

    Initial Commit

---
+++ Wedding-EntertainmentsTests/Wedding_EntertainmentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wedding_EntertainmentsTests.swift
+//  Wedding-EntertainmentsTests
+//
+//  Created by Daichi Yamaoka on 2019/03/08.
+//  Copyright © 2019 Daichi Yamaoka. All rights reserved.
+//
+
+import XCTest
+@testable import Wedding_Entertainments
+
+class Wedding_EntertainmentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
